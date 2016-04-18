0b64c307072245175963e306c6dbcfbecb267e15
huangtao800 <huangtao800@gmail.com>
2016-04-17 22:14:14 -0700

    Add std computation

--- ReadTSC/helper.h
+++ ReadTSC/helper.h
@@ -13,17 +13,6 @@
 #endif /* rdtsc_h */
 
 
-#if defined(__i386__)
-
-static inline unsigned long long rdtsc(void)
-{
-    unsigned long long int x;
-    __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
-    return x;
-}
-#elif defined(__x86_64__)
-
-
 static inline unsigned long long rdtsc(void)
 {
     unsigned hi, lo;
@@ -31,30 +20,7 @@ static inline unsigned long long rdtsc(void)
     return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
 }
 
-#elif defined(__powerpc__)
 
-
-static inline unsigned long long rdtsc(void)
-{
-    unsigned long long int result=0;
-    unsigned long int upper, lower,tmp;
-    __asm__ volatile(
-                     "0:                  \n"
-                     "\tmftbu   %0           \n"
-                     "\tmftb    %1           \n"
-                     "\tmftbu   %2           \n"
-                     "\tcmpw    %2,%0        \n"
-                     "\tbne     0b         \n"
-                     : "=r"(upper),"=r"(lower),"=r"(tmp)
-                     );
-    result = upper;
-    result = result<<32;
-    result = result|lower;
-    
-    return(result);
-}
-
-#endif
 
 static inline void func0(){}
 
@@ -69,3 +35,5 @@ static inline void func4(int a1, int a2, int a3, int a4){}
 static inline void func5(int a1, int a2, int a3, int a4, int a5){}
 
 static inline void func6(int a1, int a2, int a3, int a4, int a5, int a6){}
+
+static inline void func7(int a1, int a2, int a3, int a4, int a5, int a6, int a7){}
--- ReadTSC/main.cpp
+++ ReadTSC/main.cpp
@@ -10,6 +10,9 @@
 #include <vector>
 #include <unistd.h>
 #include <pthread.h>
+#include <functional>   // std::minus
+#include <numeric>
+#include <math.h>       /* sqrt */
 #include "helper.h"
 
 #define TIMES 1000000
@@ -17,106 +20,176 @@
 
 using namespace std;
 
-double readOverhead(){
+double computeStd(vector<uint64_t> &v, double avg){
+    double mean = avg;
+    
+    std::vector<double> diff(v.size());
+    std::transform(v.begin(), v.end(), diff.begin(),
+                   std::bind2nd(std::minus<double>(), mean));
+    double sq_sum = inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+    double stdev = sqrt(sq_sum / v.size());
+    return stdev;
+}
+
+double readOverhead(vector<uint64_t> &v){
     uint64_t start, end;
     double sum = 0;
     for(int i=0; i<TIMES;i++){
         start = rdtsc();
         end = rdtsc();
         sum += end - start;
+        v.push_back(end-start);
     }
     double avg = sum / TIMES;
     return avg;
 }
 
-double loopOverhead(){
+double loopOverhead(vector<uint64_t> &v){
     uint64_t start, end;
-    start = rdtsc();
-    for(int i=0; i<TIMES; i++){
+    double sum = 0;
+    for(int i=0;i<10;i++){
+        start = rdtsc();
+        for(int i=0; i<TIMES; i++){
+        }
+        end = rdtsc();
+        double diff = end - start;
+        double avg = diff / TIMES;
+        sum += avg;
+        v.push_back(avg);
     }
-    end = rdtsc();
-    double diff = end - start;
-    double avg = diff / TIMES;
-    return avg;
+    return sum / 10;
 }
 
-void procedureCallOverhead(vector<double> &ret){
+void procedureCallOverhead(vector<double> &ret, vector<double> &std){
     uint64_t start, end;
     uint64_t sum = 0;
+    vector<uint64_t> v;
     
+    v.clear();
     for(int i=0;i<TIMES;i++){
         start = rdtsc();
         func0();
         end = rdtsc();
         sum += (end - start);
+        v.push_back(end-start);
     }
-    ret.push_back(sum / TIMES);
+    double avg1 = sum / TIMES;
+    ret.push_back(avg1);
+    double std1 = computeStd(v, avg1);
+    std.push_back(std1);
     
     sum = 0;
+    v.clear();
     for(int i=0;i<TIMES;i++){
         start = rdtsc();
         func1(1);
         end = rdtsc();
         sum += (end - start);
+        v.push_back(end-start);
     }
-    ret.push_back(sum / TIMES);
+    double avg2 = sum / TIMES;
+    ret.push_back(avg2);
+    double std2 = computeStd(v, avg2);
+    std.push_back(std2);
     
     sum = 0;
+    v.clear();
     for(int i=0;i<TIMES;i++){
         start = rdtsc();
         func2(1, 2);
         end = rdtsc();
         sum += (end - start);
+        v.push_back(end-start);
     }
-    ret.push_back(sum / TIMES);
+    double avg3 = sum / TIMES;
+    ret.push_back(avg3);
+    double std3 = computeStd(v, avg3);
+    std.push_back(std3);
     
     sum = 0;
+    v.clear();
     for(int i=0;i<TIMES;i++){
         start = rdtsc();
         func3(1, 2, 3);
         end = rdtsc();
         sum += (end - start);
+        v.push_back(end-start);
     }
-    ret.push_back(sum / TIMES);
+    double avg4 = sum / TIMES;
+    ret.push_back(avg4);
+    double std4 = computeStd(v, avg4);
+    std.push_back(std4);
     
     sum = 0;
+    v.clear();
     for(int i=0;i<TIMES;i++){
         start = rdtsc();
         func4(1, 2, 3, 4);
         end = rdtsc();
         sum += (end - start);
+        v.push_back(end-start);
     }
-    ret.push_back(sum / TIMES);
+    double avg5 = sum / TIMES;
+    ret.push_back(avg5);
+    double std5 = computeStd(v, avg5);
+    std.push_back(std5);
     
     sum = 0;
+    v.clear();
     for(int i=0;i<TIMES;i++){
         start = rdtsc();
         func5(1, 2, 3, 4, 5);
         end = rdtsc();
         sum += (end - start);
+        v.push_back(end-start);
     }
-    ret.push_back(sum / TIMES);
+    double avg6 = sum / TIMES;
+    ret.push_back(avg6);
+    double std6 = computeStd(v, avg6);
+    std.push_back(std6);
     
     sum = 0;
+    v.clear();
     for(int i=0;i<TIMES;i++){
         start = rdtsc();
         func6(1, 2, 3, 4, 5, 6);
         end = rdtsc();
         sum += (end - start);
+        v.push_back(end-start);
     }
-    ret.push_back(sum / TIMES);
+    double avg7 = sum / TIMES;
+    ret.push_back(avg7);
+    double std7 = computeStd(v, avg7);
+    std.push_back(std7);
+    
+    sum = 0;
+    v.clear();
+    for(int i=0;i<TIMES;i++){
+        start = rdtsc();
+        func7(1, 2, 3, 4, 5, 6, 7);
+        end = rdtsc();
+        sum += (end - start);
+        v.push_back(end-start);
+    }
+    double avg8 = sum / TIMES;
+    ret.push_back(avg8);
+    double std8 = computeStd(v, avg8);
+    std.push_back(std8);
 }
 
-double systemCallOverhead(){
+double systemCallOverhead(vector<uint64_t> &v){
     uint64_t start, end;
-    double sum = 0;
+    uint64_t sum = 0;
     for(int i=0;i<TIMES;i++){
+        rdtsc();
         start = rdtsc();
         getpid();
         end = rdtsc();
         sum += end - start;
+        if(i==0)    cout << "1st iteration: " << end - start << endl;
+        v.push_back(end - start);
     }
-    double avg = sum / TIMES;
+    double avg = (double)sum / (double)TIMES;
     return avg;
 }
 
@@ -124,7 +197,7 @@ void* newThread(void *){
     pthread_exit(NULL);
 }
 
-double newThreadOverhead(){
+double newThreadOverhead(vector<uint64_t> &v){
     pthread_t t;
     uint64_t start, end;
     uint64_t sum = 0;
@@ -134,13 +207,14 @@ double newThreadOverhead(){
         end = rdtsc();
         sum += end - start;
         pthread_join(t, NULL);
+        v.push_back(end-start);
     }
     double avg = sum / CREATE_TIMES;
     return avg;
 }
 
 // Compute overhead of creating a new process
-double newProcessOverhead(){
+double newProcessOverhead(vector<uint64_t> &v){
     uint64_t start, end;
     uint64_t sum = 0;
     pid_t pid;
@@ -156,6 +230,7 @@ double newProcessOverhead(){
             end = rdtsc();
             wait(NULL);
             sum += end - start;
+            v.push_back(end-start);
         }
     }
     double avg = sum / CREATE_TIMES;
@@ -163,7 +238,7 @@ double newProcessOverhead(){
 }
 
 
-double processSwitch(){
+double processSwitch(vector<uint64_t> &v){
     uint64_t start, end;
     uint64_t sum = 0;
     int pipefd[2];
@@ -183,6 +258,7 @@ double processSwitch(){
             start = rdtsc();
             read(pipefd[1], (void *)&start, sizeof(uint64_t));
             sum += end - start;
+            v.push_back(end-start);
             wait(NULL);
         }
     }
@@ -200,7 +276,7 @@ static inline void *foo(void *) {
     pthread_exit(NULL);
 }
 
-double threadSwitch(){
+double threadSwitch(vector<uint64_t> &v){
     uint64_t start, end;
     uint64_t sum = 0;
     
@@ -212,37 +288,64 @@ double threadSwitch(){
         read(fd[0], (void *)&end, sizeof(uint64_t));
         pthread_join(thread, NULL);
         sum += end - start;
+        v.push_back(end-start);
     }
     double avg = sum / CREATE_TIMES;
     return avg;
 }
 
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    double t1 = readOverhead();
-    cout << "read overhead: " << t1 << endl;
-    
-    double t2 = loopOverhead();
-    cout << "loop overhead: " << t2 << endl;
-    
-    vector<double> times;
-    procedureCallOverhead(times);
-    for(int i=0;i<times.size();i++){
-        cout << "procedure call of " << i << " arguments overhead: " << times[i] << endl;
-    }
-    
-    double t4 = systemCallOverhead();
-    cout << "system call overhead: " << t4 << endl;
+    vector<uint64_t> v;
+//    double t1 = readOverhead(v);
+//    cout << "read overhead: " << t1 << endl;
+//    double std1 = computeStd(v, t1);
+//    cout << "read std: " << std1 << endl;
 
-    double t5 = newThreadOverhead();
-    cout << "new thread overhead: "<< t5 << endl;
+//    v.clear();
+//    double t2 = loopOverhead(v);
+//    cout << "loop overhead: " << t2 << endl;
+//    double std2 = computeStd(v, t2);
+//    cout << "loop std: " << std2 << endl;
+
+//    vector<double> times;
+//    vector<double> std;
+//    procedureCallOverhead(times, std);
+//    for(int i=0;i<times.size();i++){
+//        cout << "procedure call of " << i << " arguments overhead: " << times[i] << endl;
+//        cout << "procedure call of " << i << " arguments std: " << std[i] << endl;
+//    }
     
-    double t6 = newProcessOverhead();
+//    v.clear();
+//    double t4 = systemCallOverhead(v);
+//    double std4 = computeStd(v, t4);
+//    cout << "system call overhead: " << t4 << endl;
+//    cout << "system call std: " << std4 << endl;
+//    
+//   
+//    
+//    v.clear();
+//    double t5 = newThreadOverhead(v);
+//    cout << "new thread overhead: "<< t5 << endl;
+//    double std5 = computeStd(v, t5);
+//    cout << "new thread std: " << std5 << endl;
+//
+    v.clear();
+    double t6 = newProcessOverhead(v);
     cout << "new process overhead: " << t6 << endl;
+    double std6 = computeStd(v, t6);
+    cout << "new process std: " << std6 << endl;
+//
+//    v.clear();
+//    double t7 = processSwitch(v);
+//    cout << "process context switch overhead: " << t7 << endl;
+//    double std7 = computeStd(v, t7);
+//    cout << "process context switch std: " << std7 << endl;
     
-    double t7 = processSwitch();
-    cout << "process context switch overhead: " << t7 << endl;
-    
-    double t8 = threadSwitch();
-    cout << "thread context switch overhead: " << t8 << endl;
+//    v.clear();
+//    double t8 = threadSwitch(v);
+//    cout << "thread context switch overhead: " << t8 << endl;
+//    double std8 = computeStd(v, t8);
+//    cout << "thread context switch std: " << std8 << endl;
 }
